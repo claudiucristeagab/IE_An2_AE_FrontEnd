11950b46c2313742807b672f5469090288e36101
Ubuntu <ubuntu@ip-172-31-23-240.us-east-2.compute.internal>
2020-01-12 20:26:07 +0000

    quantity

---
+++ frontend/src/components/ProductQuantityCount.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+export default class ProductQuantityCount extends Component {
+    constructor(props){
+        super(props);
+        this.state = {quantity: this.props.quantity};
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+    }
+    
+    increment() {
+        const quantity = this.state.quantity;
+        this.setState({ quantity: quantity + 1 });
+        this.props.action(quantity+1);
+    }
+    
+    decrement() {
+        const quantity = this.state.quantity;
+        if (quantity>1) {
+            this.setState({ quantity: quantity - 1 });
+            this.props.action(quantity-1);
+        }
+    }
+    
+    render() {
+        const quantity = this.state.quantity;
+        return (
+            <div className="input-group">
+                <span className="input-group-btn">
+                    <button className="btn btn-white btn-minuse" type="button" onClick={this.decrement}>-</button>
+                </span>
+                <input type="text" className="form-control no-padding add-color text-center height-25" maxlength="3" value={quantity}/>
+                <span className="input-group-btn">
+                    <button className="btn btn-red btn-pluss" type="button" onClick={this.increment}>+</button>
+                </span>
+            </div>
+        )
+    }
+    
+}
--- frontend/src/components/ProductView.js
+++ frontend/src/components/ProductView.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import {API_BASEURL} from '../constants/apiConstants';
+import ProductQuantityCount from './ProductQuantityCount';
 
 export default class ProductView extends Component {
     constructor(props){
         super(props);
-        this.state = {product: ""}
+        this.state = {product: "", quantity: 1};
+        this.getProduct = this.getProduct.bind(this);
+        this.handleTotal = this.handleTotal.bind(this);
     }
     
     componentDidMount() {
@@ -14,6 +17,7 @@ export default class ProductView extends Component {
     
     render() {
         const product = this.state.product;
+        const total = this.state.quantity * product.price;
         return (
             <div className='container-md'>
                 <div className='row h-50'>
@@ -24,18 +28,25 @@ export default class ProductView extends Component {
                         <div>
                             <h3>{product.name}</h3>
                             <p>{product.description}</p>
-                        </div>
-                        <hr/>
-                        <div>
                             <div class="row">
                                 <div className="col">
                                     <span>Price: ${product.price}</span>
                                 </div>
                                 <div className="col">
+                                    <span>Total Price: ${total}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div>
+                            <div class="row">
+                                <div className="col">
+                                    <ProductQuantityCount quantity={this.state.quantity} action={this.handleTotal}/>
+                                </div>
+                                <div className="col">
                                     <a className="btn btn-primary btn-block" href="#" role="button">Buy</a>
                                 </div>
                             </div>
-                            
                         </div>
                     </div>
                 </div>
@@ -48,6 +59,10 @@ export default class ProductView extends Component {
         .then(data => this.setState({
             product: data
         }));
+    }
+    
+    handleTotal(i){
+        this.setState({ quantity: i });
     }
     
 }
